f0bb717cb09eb80921772c5431cd7a589857710d
Isaac Murchie and Danielle Levinson <student@appacademy.io>
2013-09-23 16:54:45 -0700

    Pre code-review

--- mine_sweeper.rb
+++ mine_sweeper.rb
@@ -11,32 +11,28 @@ module MineSweeper
       @size = size
       @bomb_count = (size == SMALL ? 10 : 40)
 
-      bombs = bomb_placement
-      @grid = []
-      (0...@size).each do |x|
-        row = []
-        (0...@size).each do |y|
-          row << Tile.new(x, y, bomb: bombs.include?([x, y]))
-        end
-
-        @grid << row
-      end
+      @grid = init_grid
     end
 
     def display
-      puts "1 2 3 4 5 6 7 8 @size"
+      puts "1 2 3 4 5 6 7 8 9"
       (0...@size).each do |x|
         (0...@size).each do |y|
-          if @grid[x][y].visible?
-            if @grid[x][y].adjacent_bomb_count == 0
+          tile = @grid[x][y]
+
+          if tile.visible?
+            if tile.bomb?
+              print "B"
+            elsif tile.adjacent_bomb_count == 0
               print "_"
             else
-              print @grid[x][y].adjacent_bomb_count
+              print tile.adjacent_bomb_count
             end
           else
-            #for debugging
-            if @grid[x][y].bomb?
-              print "b"
+            if tile.flagged?
+              print "F"
+            elsif tile.bomb? && game_over?
+              print "B"
             else
               print "*"
             end
@@ -51,6 +47,8 @@ module MineSweeper
 
     def reveal(pos)
       tile = @grid[pos[0]][pos[1]]
+      return if tile.flagged?
+
       if tile.bomb?
         tile.visible = true
         @game_over = true
@@ -58,6 +56,11 @@ module MineSweeper
       end
 
       explore(tile)
+    end
+
+    def flag(pos)
+      tile = @grid[pos[0]][pos[1]]
+      tile.flag = !tile.flagged?
     end
 
     def game_over?
@@ -79,6 +82,22 @@ module MineSweeper
 
 
     private
+    def init_grid
+      bombs = bomb_placement
+      p bombs
+      grid = []
+      (0...@size).each do |x|
+        row = []
+        (0...@size).each do |y|
+          row << Tile.new(x, y, bomb: bombs.include?([x, y]))
+        end
+
+        grid << row
+      end
+
+      grid
+    end
+
     def bomb_placement
       bombs = []
       while bombs.length < @bomb_count
@@ -104,7 +123,7 @@ module MineSweeper
 
         if tile_info[:bomb_count] == 0
           tile_info[:tiles].each do |tile|
-            if !queue.include?(tile) && !tile.visible?
+            if !queue.include?(tile) && !tile.visible? && !tile.flagged?
               queue << tile
             end
           end
@@ -114,8 +133,6 @@ module MineSweeper
 
     def adjacent_tile_info(tile)
       tile_info = {}
-
-      legal_tiles = []
 
       offsets      = [ [0, 1],
                        [0, -1],
@@ -127,48 +144,48 @@ module MineSweeper
                        [1, -1],
                        [1, 1] ]
 
-      bomb_count = 0
+      tile_info[:tiles] = adjacent_tiles(tile, offsets)
+
+      tile_info[:bomb_count] = bomb_count(tile, offsets + diag_offsets)
+
+      tile_info
+    end
+
+    def adjacent_tiles(tile, offsets)
+      legal_tiles = []
       offsets.each do |x, y|
         new_x = tile.x - x
         new_y = tile.y - y
 
         if (new_x >= 0 && new_x < @size) && (new_y >= 0 && new_y < @size)
-          ad_tile = @grid[new_x][new_y]
-
-          legal_tiles << ad_tile
-          bomb_count += 1 if ad_tile.bomb?
-        end
-      end
-      tile_info[:tiles] = legal_tiles
-
-      count_tiles = []
-      diag_offsets.each do |x, y|
-        new_x = tile.x - x
-        new_y = tile.y - y
-        if (new_x >= 0 && new_x < @size) && (new_y >= 0 && new_y < @size)
-          bomb_count += 1 if @grid[new_x][new_y].bomb?
-          count_tiles << @grid[new_x][new_y]
+          legal_tiles << @grid[new_x][new_y]
         end
       end
 
-      tile_info[:bomb_count] = bomb_count
-
-      tile_info
+      legal_tiles
     end
 
-    def count_bombs(legal_tiles)
+    def bomb_count(tile, offsets)
       count = 0
-      legal_tiles.each do |tile|
-        count += 1 if tile.bomb?
+      offsets.each do |x, y|
+        new_x = tile.x - x
+        new_y = tile.y - y
+
+        if (new_x >= 0 && new_x < @size) && (new_y >= 0 && new_y < @size)
+          count += 1 if @grid[new_x][new_y].bomb?
+        end
       end
 
       count
     end
   end
 
+
+
   class Tile
     attr_accessor :visible, :adjacent_bomb_count
     attr_reader :x, :y
+    attr_writer :flag
 
     def initialize(x, y, options = {})
       @x = x
@@ -176,6 +193,7 @@ module MineSweeper
       @bomb = options[:bomb]
       @visible = false
       @adjacent_bomb_count = nil
+      @flag = false
     end
 
     def bomb?
@@ -184,6 +202,10 @@ module MineSweeper
 
     def visible?
       @visible
+    end
+
+    def flagged?
+      @flag
     end
 
     def to_s
@@ -202,8 +224,12 @@ module MineSweeper
       @board.display
 
       until @board.game_over?
-        position = get_user_input
-        @board.reveal(position)
+        action, position = get_user_input
+        if action == "r"
+          @board.reveal(position)
+        else
+          @board.flag(position)
+        end
         @board.display
       end
 
@@ -218,9 +244,14 @@ module MineSweeper
     private
 
     def get_user_input
-      puts "Enter the click you would like to make (format x y)"
-      user_input = gets.chomp.split(' ').map { |i| (i.to_i - 1) }
-      user_input.reverse
+      puts "Reveal (r) or Flag (f) a square?"
+      print "Enter the click you would like to make (format: [rf] x y): "
+      user_input = gets.chomp.split(' ')
+
+      action = user_input[0]
+      position = user_input[1..-1].map { |i| i.to_i - 1 }.reverse
+
+      [action, position]
     end
   end
 end
